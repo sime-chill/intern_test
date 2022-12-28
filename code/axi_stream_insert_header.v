7e0709d8eb09d24e029463ec53331429f6813ade
yuhan <heyh39@mail2.sysu.edu.cn>
2022-12-28 20:42:52 +0800

    reconstruct axi header insertor

--- code/axi_stream_insert_header.v
+++ code/axi_stream_insert_header.v
@@ -13,8 +13,8 @@ module axi_stream_insert_header
     input                             last_in,
     output reg                        ready_in,
 // AXI Stream output with header inserted
-    output                            valid_out,
-    output reg [DATA_WD - 1 : 0]      data_out,
+    output reg                        valid_out,
+    output     [DATA_WD - 1 : 0]      data_out,
     output reg [DATA_BYTE_WD - 1 : 0] keep_out,
     output                            last_out,
     input                             ready_out,
@@ -25,277 +25,107 @@ module axi_stream_insert_header
     output reg                        ready_insert
   );
 
-  reg                         last_reg;
+  localparam                       KEEP_WD           = $clog2(DATA_BYTE_WD);
 
-  reg                         valid_insert_reg;  //this signal will keep high from valid_insert is high to valid_out is low
+//handshake
+  wire                             header_fire;
+  assign header_fire = ready_insert && valid_insert;
+  wire                             data_fire;
+  assign data_fire   = ready_in && valid_in;                                          //fire only decides on valid and ready
+
+
+  reg                              last_reg;
+  always @(posedge clk) last_reg <= (!rst_n) ? 0 : last_in;
+
+  reg                              valid_insert_reg;                        //this signal will keep high from valid_insert is high to valid_out is low
   always @(posedge clk) begin
     if(!rst_n) valid_insert_reg            <= 0;
     else if(valid_insert) valid_insert_reg <= 1;
     else if(last_out) valid_insert_reg     <= 0;
   end
 
-  wire                        header_succ;
-  assign header_succ  = ready_insert & valid_insert;
-  wire                        data_in_succ;
-  assign data_in_succ = ready_in & valid_in & ready_out; //only the data can move out, new data come in
-  assign valid_out    = data_in_succ | last_out;         //the cycle after the last_in should have valid_out
-
-  reg                         insert_shake_once; //to confirm that insert_ready is high only 1 cycle
+  reg                              insert_shake_once;                       //to confirm that insert_ready is high only 1 cycle
   always @(posedge clk) begin
     if(!rst_n) insert_shake_once           <= 0;
-    else if(header_succ) insert_shake_once <= 1;
+    else if(header_fire) insert_shake_once <= 1;
     else if(last_out) insert_shake_once    <= 0;
   end
 
   always @(*) begin //assert ready_insert
-    if(~valid_in | insert_shake_once) ready_insert = 0;
-    else if(last_reg) ready_insert                 = 0;
-    else ready_insert                              = 1;
+    if(!valid_in || insert_shake_once) ready_insert = 0;
+    else if(last_reg) ready_insert                  = 0;
+    else ready_insert                               = 1;
   end
 
   always @(*) begin //assert ready_in
-    if(~ready_out) ready_in                              = 0;
-    else if(~(valid_insert | valid_insert_reg)) ready_in = 0;
-    else if(last_reg) ready_in                           = 0;
-    else ready_in                                        = 1;
+    if(!ready_out) ready_in                               = 0; //TODO: how to use ready_out?
+    else if(!(valid_insert || valid_insert_reg)) ready_in = 0;
+    else if(last_reg) ready_in                            = 0;
+    else ready_in                                         = 1;
   end
 
-  reg  [DATA_WD - 1 : 0]      data_reg;
-  always @(posedge clk) begin
-    if(!rst_n) data_reg <= 0;
-    else data_reg       <= data_in;
-  end
+  reg        [KEEP_WD : 0]         data_count;
+  reg        [KEEP_WD : 0]         data_count_reg;
+  reg        [KEEP_WD : 0]         header_count_reg;                        //the number of exchange bytes
+  reg        [KEEP_WD : 0]         header_count;
+  reg                              last_next;
 
-  reg  [DATA_BYTE_WD - 1 : 0] keep_reg;
-  always @(posedge clk) begin
-    if(!rst_n) keep_reg <= 0;
-    else keep_reg       <= keep_in;
-  end
-
-  reg  [2 : 0]                count;             //the number of exchange bytes
-  reg                         last_next;
-
-  //parameter
-  always @(posedge clk) begin //when exchange header byte, set the count and reset the count when exchange next time
-    if(!rst_n) count <= 0;
-    else if(header_succ & data_in_succ) begin
-      case (keep_insert)
-        4'b1111 : begin
-          count <= 4;
-        end
-        4'b0111 : begin
-          count <= 3;
-        end
-        4'b0011 : begin
-          count <= 2;
-        end
-        4'b0001 : begin
-          count <= 1;
-        end
-        4'b0000 : begin
-          count <= 0;
-        end
-        default : begin
-          count <= 0;
-        end
-      endcase
+  integer                          i, j;                                    //implement a priority encoder with for
+  always @(*) begin
+    data_count = 0;
+    for(i = DATA_BYTE_WD - 1; i >= 0; i = i - 1) begin
+      if(keep_in[i]) data_count = DATA_BYTE_WD - i;
     end
   end
+  always @(posedge clk) data_count_reg <= (!rst_n) ? 0 : data_count;
 
   always @(*) begin
-    if(header_succ & data_in_succ) begin            //exchange header
-      keep_out  = 4'b1111;
-      last_next = 0;
-      case (keep_insert)
-        4'b1111 : begin
-          data_out = header_insert;
-        end
-        4'b0111 : begin
-          data_out = {header_insert[23 : 0], data_in[31 : 24]};
-        end
-        4'b0011 : begin
-          data_out = {header_insert[15 : 0], data_in[31 : 16]};
-        end
-        4'b0001 : begin
-          data_out = {header_insert[7 : 0], data_in[31 : 8]};
-        end
-        4'b0000 : begin
-          data_out = data_in;
-        end
-        default : begin
-          data_out = 32'b0;
-        end
-      endcase
-    end
-
-    else if(data_in_succ & last_in & ~|count) begin //header no exchange
-      data_out  = data_in;
-      keep_out  = keep_in;
-      last_next = 0;
-    end
-
-    else if(data_in_succ & last_in & |count) begin  //last_in is high, decide data_out this cycle, so what about next cycle?
-      if(count == 3'd4) begin
-        data_out  = data_reg;
-        keep_out  = 4'b1111;
-        last_next = 1;
-      end
-      else if(count == 3'd3) begin
-        data_out  = {data_reg[23 : 0], data_in[31 : 24]};
-        keep_out  = 4'b1111;
-        last_next = 1;
-      end
-      else if(count == 3'd2) begin
-        case (keep_in)
-          4'b1111 : begin
-            data_out  = {data_reg[15 : 0], data_in[31 : 16]};
-            last_next = 1;
-            keep_out  = 4'b1111;
-          end
-          4'b1110 : begin
-            data_out  = {data_reg[15 : 0], data_in[23 : 8]};
-            last_next = 1;
-            keep_out  = 4'b1111;
-          end
-          4'b1100 : begin
-            data_out  = {data_reg[15 : 0], data_in[15 : 0]};
-            last_next = 1;
-            keep_out  = 4'b1111;
-          end
-          4'b1000 : begin
-            data_out  = {data_reg[15 : 0], data_in[7 : 0], 8'b0};
-            last_next = 0;
-            keep_out  = 4'b1110;
-          end
-          default : begin
-            data_out  = data_out;
-            last_next = 0;
-            keep_out  = 4'b0000;
-          end
-        endcase
-      end
-      else if(count == 3'd1) begin
-        case (keep_in)
-          4'b1111 : begin
-            data_out  = {data_reg[7 : 0], data_in[31 : 8]};
-            keep_out  = 4'b1111;
-            last_next = 1;
-          end
-          4'b1110 : begin
-            data_out  = {data_reg[7 : 0], data_in[23 : 0]};
-            keep_out  = 4'b1111;
-            last_next = 1;
-          end
-          4'b1100 : begin
-            data_out  = {data_reg[7 : 0], data_in[15 : 0], 8'b0};
-            keep_out  = 4'b1110;
-            last_next = 0;
-          end
-          4'b1000 : begin
-            data_out  = {data_reg[7 : 0], data_in[7 : 0], 16'b0};
-            keep_out  = 4'b1100;
-            last_next = 0;
-          end
-          default : begin
-            data_out  = data_out;
-            keep_out  = 4'b0000;
-            last_next = 0;
-          end
-        endcase
-      end
-      else begin
-        data_out  = data_out;
-        keep_out  = 4'b0000;
-        last_next = 0;
-      end
-    end
-
-    else if(last_reg) begin                         //last_in is high, this branch is next cycle
-      last_next = 1;
-      if(count == 3'd4) begin
-        data_out  = data_reg;
-        keep_out  = keep_reg;
-      end
-      else if(count == 3'd3) begin
-        case (keep_reg)
-          4'b1111 : begin
-            data_out = {data_reg[23 : 0], 8'b0};
-            keep_out = 4'b1110;
-          end
-          4'b1110 : begin
-            data_out = {data_reg[23 : 8], 16'b0};
-            keep_out = 4'b1100;
-          end
-          4'b1100 : begin
-            data_out = {data_reg[23 : 16], 24'b0};
-            keep_out = 4'b1000;
-          end
-          default : begin
-            data_out = 32'b0;
-            keep_out = 4'b0000;
-          end
-        endcase
-      end
-      else if(count == 3'd2) begin
-        case (keep_reg)
-          4'b1111 : begin
-            data_out = {data_reg[15 : 0], 16'b0};
-            keep_out = 4'b1100;
-          end
-          4'b1110 : begin
-            data_out = {data_reg[15 : 8], 24'b0};
-            keep_out = 4'b1000;
-          end
-          default : begin
-            data_out = 32'b0;
-            keep_out = 4'b0000;
-          end
-        endcase
-      end
-      else if(count == 3'd1) begin
-        case (keep_reg)
-          4'b1111 : begin
-            data_out = {data_reg[7 : 0], 24'b0};
-            keep_out = 4'b1000;
-          end
-          default : begin
-            data_out = 32'b0;
-            keep_out = 4'b0000;
-          end
-        endcase
-      end
-      else begin
-        data_out  = 32'b0;
-        keep_out  = 4'b0000;
-      end
-    end
-
-    else if(data_in_succ) begin                     //exchange the middle bytes
-      keep_out  = 4'b1111;
-      last_next = 0;
-      case (count)
-        3'd4 : data_out    = data_reg;
-        3'd3 : data_out    = {data_reg[23 : 0], data_in[31 : 24]};
-        3'd2 : data_out    = {data_reg[15 : 0], data_in[31 : 16]};
-        3'd1 : data_out    = {data_reg[7 : 0], data_in[31 : 8]};
-        3'd0 : data_out    = data_in;
-        default : data_out = data_in;
-      endcase
-    end
-
-    else begin                                      //for other circumstances, reset all outputs
-      data_out  = 32'b0;
-      keep_out  = 4'b0;
-      last_next = 0;
+    header_count = 0;
+    for(j = 0; j < DATA_BYTE_WD; j = j + 1) begin
+      if(keep_insert[j]) header_count = j + 1;
     end
   end
 
   always @(posedge clk) begin
-    if(!rst_n) last_reg                   <= 0;
-    else if(last_in & last_next) last_reg <= last_in;
-    else last_reg                         <= 0;
+    if(!rst_n) header_count_reg           <= 0;
+    else if(header_fire) header_count_reg <= header_count;
+    else if(last_out) header_count_reg    <= 0;
   end
-  assign last_out     = last_next ? last_reg : data_in_succ ? last_in : 0;
 
+  always @(posedge clk) begin
+    if(!rst_n) last_next       <= 0;
+    else if(last_in) last_next <= header_count_reg + data_count > DATA_BYTE_WD ? 1 : 0;
+    else last_next             <= 0;
+  end
+  reg                              last_next_1;                             //delay 1 beat
+  always @(posedge clk) last_next_1 <= (!rst_n) ? 0 : last_next;
+  assign last_out    = last_reg && !last_next ? last_reg : last_next_1;
+
+  always @(posedge clk) begin
+    if(!rst_n) keep_out         <= 0;
+    else if(last_in) keep_out   <= (header_count_reg + data_count >= DATA_BYTE_WD) ? {DATA_BYTE_WD{1'b1}} : {$signed(keep_in) >>> header_count_reg};
+    else if(data_fire) keep_out <= {DATA_BYTE_WD{1'b1}};
+    else if(last_next) keep_out <= 4'sb1000 >>> (header_count_reg + data_count_reg - DATA_BYTE_WD - 1); //data_out is 1 beat longer
+    else keep_out               <= 0;
+  end
+
+  always @(posedge clk) begin
+    if(!rst_n) valid_out         <= 0;
+    else if(data_fire) valid_out <= 1;
+    else if(last_next) valid_out <= 1;
+    else if(last_out) valid_out  <= 0;
+  end
+
+  reg        [2 * DATA_WD - 1 : 0] shift_reg;
+  always @(posedge clk) begin
+    if(!rst_n) shift_reg                      <= 0;
+    else if(header_fire) shift_reg            <= {header_insert, data_in};
+    else if(data_fire || last_next) shift_reg <= {shift_reg[DATA_WD - 1 : 0], data_in};
+    else shift_reg                            <= 0;
+  end
+
+  wire       [2 * DATA_WD - 1 : 0] shift_res;
+  assign shift_res   = shift_reg << ({3'b0, (DATA_BYTE_WD - header_count_reg)} << 3); //add 3 bits width to shift
+  assign data_out    = shift_res[2 * DATA_WD - 1 : DATA_WD];
 
 endmodule
--- code/tb.sv
+++ code/tb.sv
@@ -13,7 +13,7 @@ module tb
 
   reg                        clk, rst_n;
   initial begin
-    clk       = 1;
+    clk       = 0;
     rst_n     = 0;
     ready_out = 1;
     #20 rst_n = 1;
@@ -63,6 +63,58 @@ module tb
 //  end
 
 //ex2: 1st beat of data is faster than header
+//  initial begin
+//    valid_in      <= 0;
+//    last_in       <= 0;
+//    keep_in       <= 4'b0;
+//    data_in       <= 32'h0;
+//    ready_out     <= 1;
+//    valid_insert  <= 0;
+//    header_insert <= 32'h0;
+//    keep_insert   <= 4'b0000;
+//    #105;
+//    valid_in      <= 1;
+//    keep_in       <= 4'b1111;
+//    valid_insert  <= 0;
+//    header_insert <= 32'h0;
+//    data_in       <= 32'hAABB_CCDD;
+//    keep_insert   <= 4'b0;
+//    #10;
+//    valid_in      <= 1;
+//    valid_insert  <= 0;
+//    header_insert <= 32'h0;
+//    data_in       <= 32'hAABB_CCDD;
+//    keep_insert   <= 4'b0;
+//    #10;
+//    valid_in      <= 1;
+//    valid_insert  <= 1;
+//    header_insert <= 32'hFFEE_DDCC;
+//    data_in       <= 32'hAABB_CCDD;
+//    keep_insert   <= 4'b0001;
+//    #10;
+//    valid_in      <= 1;
+//    valid_insert  <= 0;
+//    header_insert <= 32'h0;
+//    data_in       <= 32'hEEFF_0011;
+//    keep_insert   <= 4'b0;
+//    #10;
+//    valid_insert  <= 0;
+//    data_in       <= 32'h2233_4455;
+//    #10;
+//    valid_insert  <= 0;
+//    data_in       <= 32'h6677_8899;
+//    #10;
+//    data_in       <= 32'h00AA_BBCC;
+//    last_in       <= 1;
+//    keep_in       <= 4'b1100;
+//    #10;
+//    keep_in       <= 4'b0000;
+//    valid_in      <= 0;
+//    last_in       <= 0;
+//    #20 $finish;
+//  end
+
+//ex3: 1st beat of data is slower than header
   initial begin
     valid_in      <= 0;
     last_in       <= 0;
@@ -73,20 +125,21 @@ module tb
     header_insert <= 32'h0;
     keep_insert   <= 4'b0000;
     #105;
+    valid_in      <= 0;
+    keep_in       <= 4'b0;
+    valid_insert  <= 1;
+    header_insert <= 32'hFFEE_DDCC;
+    data_in       <= 32'h0;
+    keep_insert   <= 4'b0111;
+    #10;
+    valid_in      <= 0;
+    valid_insert  <= 1;
+    header_insert <= 32'hFFEE_DDCC;
+    data_in       <= 32'h0;
+    keep_insert   <= 4'b0111;
+    #10;
     valid_in      <= 1;
     keep_in       <= 4'b1111;
-    valid_insert  <= 0;
-    header_insert <= 32'h0;
-    data_in       <= 32'hAABB_CCDD;
-    keep_insert   <= 4'b0;
-    #10;
-    valid_in      <= 1;
-    valid_insert  <= 0;
-    header_insert <= 32'h0;
-    data_in       <= 32'hAABB_CCDD;
-    keep_insert   <= 4'b0;
-    #10;
-    valid_in      <= 1;
     valid_insert  <= 1;
     header_insert <= 32'hFFEE_DDCC;
     data_in       <= 32'hAABB_CCDD;
@@ -113,59 +166,6 @@ module tb
     last_in       <= 0;
     #20 $finish;
   end
-
-//ex3: 1st beat of data is slower than header
-//  initial begin
-//    valid_in      <= 0;
-//    last_in       <= 0;
-//    keep_in       <= 4'b0;
-//    data_in       <= 32'h0;
-//    ready_out     <= 1;
-//    valid_insert  <= 0;
-//    header_insert <= 32'h0;
-//    keep_insert   <= 4'b0000;
-//    #105;
-//    valid_in      <= 0;
-//    keep_in       <= 4'b0;
-//    valid_insert  <= 1;
-//    header_insert <= 32'hFFEE_DDCC;
-//    data_in       <= 32'h0;
-//    keep_insert   <= 4'b0111;
-//    #10;
-//    valid_in      <= 0;
-//    valid_insert  <= 1;
-//    header_insert <= 32'hFFEE_DDCC;
-//    data_in       <= 32'h0;
-//    keep_insert   <= 4'b0111;
-//    #10;
-//    valid_in      <= 1;
-//    keep_in       <= 4'b1111;
-//    valid_insert  <= 1;
-//    header_insert <= 32'hFFEE_DDCC;
-//    data_in       <= 32'hAABB_CCDD;
-//    keep_insert   <= 4'b0111;
-//    #10;
-//    valid_in      <= 1;
-//    valid_insert  <= 0;
-//    header_insert <= 32'h0;
-//    data_in       <= 32'hEEFF_0011;
-//    keep_insert   <= 4'b0;
-//    #10;
-//    valid_insert  <= 0;
-//    data_in       <= 32'h2233_4455;
-//    #10;
-//    valid_insert  <= 0;
-//    data_in       <= 32'h6677_8899;
-//    #10;
-//    data_in       <= 32'h00AA_BBCC;
-//    last_in       <= 1;
-//    keep_in       <= 4'b1100;
-//    #10;
-//    keep_in       <= 4'b0000;
-//    valid_in      <= 0;
-//    last_in       <= 0;
-//    #20 $finish;
-//  end
 
 //  reg [9 : 0]                counter;
 //  always @(posedge clk) begin
